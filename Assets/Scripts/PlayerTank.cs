7165129187615bfb6b34e57bb8a2713e967748dc
X00136708 <x00136708@gmail.com>
2022-08-29 16:20:52 +0100

    increased folder structure + new left & right  sprites for tanks

--- Assets/Scripts/PlayerTank.cs
+++ Assets/Scripts/PlayerTank.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tank : MonoBehaviour
+public class PlayerTank : Tank
 {
     // Start is called before the first frame update
-    Vector2 Vec;
+    Tank playerTank = new PlayerTank();
     void Start()
     {
         
@@ -13,6 +13,10 @@ public class Tank : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+    public override void Move()
     {
         if (Input.GetKey(KeyCode.A))
         {
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# TankWarfare
+Tank Warfare 
+based on shellshock 
+
+
+
+1 player game (multiplayer)
+shooting system 
+movement 
+currency 
+lives 
+score 
+health/health bar 
+customizable tank
+object to block shots 
+power ups 
