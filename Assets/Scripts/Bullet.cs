8635634143b9c8e4e14cdc3a7c00d4b7bf461467
X00136708 <x00136708@gmail.com>
2022-08-29 17:38:24 +0100

    added Bullet generic functionality

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public abstract class Bullet : MonoBehaviour
+    {
+    public float speed;
+    public int damage;
+    public string type;    
+
+    protected Bullet(float speed, int damage, string type)
+    {
+        this.speed = speed;
+        this.damage = damage;
+        this.type = type;
+    }
+
+    public string Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+        }
+    }        
+        public abstract void Shot();
+        public abstract void Hit();
+
+    }
+
---
+++ Assets/Scripts/PlayerDefaultShot.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class PlayerDefaultShot : Bullet
+{
+    Bullet bullet = new PlayerDefaultShot(20f, 100, "DefaultShot");
+    public PlayerDefaultShot(float speed, int damage, string type) : base(speed, damage, type)
+    {
+        bullet.speed = speed;
+        bullet.damage = damage;
+        bullet.type = type;
+    }
+    public override void Shot()
+    {
+
+    }
+    public override void Hit()
+    {
+        
+    }    
+}
+
