91f8a1cb4f6199451a43afa2ffcd653427573fc3
X00136708 <x00136708@gmail.com>
2022-08-29 16:58:05 +0100

    Updated bot tank barrell location

--- Assets/Scripts/PlayerTankBarrel.cs
+++ Assets/Scripts/PlayerTankBarrel.cs
@@ -18,12 +18,12 @@ using UnityEngine;
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Rotate(0, 0, Time.deltaTime);
+            this.transform.Rotate(0, 0, Time.deltaTime*5);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Rotate(0, 0, Time.deltaTime);
+            this.transform.Rotate(0, 0, Time.deltaTime*-5);
         }
     }
     public override void ExtendBarrel()
